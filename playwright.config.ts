faedaceb8308538b79ab66ee4978d30a9137c188
Rafal Krysiak <rafal@near-me.com>
2025-11-27 22:30:00 +0100

    Remove projects for Firefox and Safari - they are not needed

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,8 +5,6 @@ export default defineConfig({
   reporter: [['html', { open: 'never', host: '0.0.0.0', port: 9323 }]],
   use: { trace: 'on-first-retry', screenshot: 'only-on-failure', video: 'retain-on-failure' },
   projects: [
-    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
-    { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
-    { name: 'webkit', use: { ...devices['Desktop Safari'] } }
+    { name: 'chromium', use: { ...devices['Desktop Chrome'] } }
   ]
 });
